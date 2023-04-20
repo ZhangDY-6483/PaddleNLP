fb047c6015199c078bef49746da05c6f8b07b470
ZhangDY-6483 <zhangdanyang6483@163.com>
2023-04-20 18:47:32 +0800

    add config of 10/65B

--- model_zoo/gpt-3/run_singlecard.sh
+++ model_zoo/gpt-3/run_singlecard.sh
@@ -7,7 +7,6 @@ export NVIDIA_TF32_OVERRIDE=0
 rm -rf ./data/*indexmap*
 rm -rf ./log/*
 
-DISTRIBUTED_ARGS="--nnodes=2 --master=10.95.147.140:36712"
-
-python -m paddle.distributed.launch --gpus 0,1,2,3,4,5,6,7 $DISTRIBUTED_ARGS ./tools/train.py -c ./ppfleetx/configs/nlp/gpt/pretrain_gpt_10B_mp8_pp2.yaml
+# export LD_LIBRARY_PATH=/usr/local/cuda/compat:$LD_LIBRARY_PATH
+python -m paddle.distributed.launch --gpus 0,1,2,3,4,5,6,7 ./tools/train.py -c ./ppfleetx/configs/nlp/gpt/pretrain_gpt_10B_mp8_pp1.yaml
 
